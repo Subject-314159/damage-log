60e64e836089c4da655a3e8dad0cb4e8401c127a
Subject-314159 <168562337+Subject-314159@users.noreply.github.com>
2026-02-10 21:44:50 +0100

    wip

--- control.lua
+++ control.lua
@@ -21,10 +21,8 @@ script.on_event(defines.events.on_tick, function(e)
 end)
 
 script.on_event(defines.events.on_entity_damaged, function(e)
+    -- This is also triggered if a unit died
     game.print("(" .. game.tick .. ") " .. e.entity.name .. " damaged, remaining health: " ..
                    serpent.line(e.final_health))
-end)
-script.on_event(defines.events.on_object_destroyed, function(e)
-    game.print("(" .. game.tick .. ") " .. e.type .. " got destroyed")
 end)
 
