e1b443ff9dc5796771cc92d592dbffe49e97154e
Subject-314159 <168562337+Subject-314159@users.noreply.github.com>
2026-02-12 09:45:46 +0100

    wip

--- control.lua
+++ control.lua
@@ -1,5 +1,5 @@
 -- Data model
-
+local damage = require("model.damage")
 
 local init = function()
     -- Ensure data model
@@ -10,6 +10,37 @@ local init = function()
 end
 
 local load = function()
+    commands.add_command("show-dmg", "Mockup to show the damage", function(command)
+        local p = game.get_player(command.player_index)
+        local map = damage.get(p.force.index, p.surface.index, {{-100, -100}, {100, 100}})
+
+        -- First get the max val
+        local max = 0
+        for x, col in pairs(map or {}) do
+            for y, val in pairs(col or {}) do
+                if val > 1 then
+                    max = math.max(max, val)
+                end
+            end
+        end
+
+        -- Then draw the heatmap
+        for x, col in pairs(map or {}) do
+            for y, val in pairs(col or {}) do
+                if val > 1 then
+                    local prop = {
+                        color = {1, 0, 0, (val / max)},
+                        filled = true,
+                        left_top = {x, y},
+                        right_bottom = {x + 1, y + 1},
+                        surface = p.surface.index,
+                        time_to_live = 10 * 60
+                    }
+                    rendering.draw_rectangle(prop)
+                end
+            end
+        end
+    end)
 end
 
 script.on_configuration_changed(function()
@@ -25,12 +56,21 @@ script.on_load(function()
     load()
 end)
 
+script.on_event({defines.events.on_player_joined_game, defines.events.on_player_created,
+                 defines.events.on_player_changed_force}, function(e)
+    init()
+end)
 script.on_event(defines.events.on_tick, function(e)
 end)
 
 script.on_event(defines.events.on_entity_damaged, function(e)
-    -- This is also triggered if a unit died
-    game.print("(" .. game.tick .. ") " .. e.entity.name .. " damaged, remaining health: " ..
-                   serpent.line(e.final_health))
+    -- Early exit if it's not in our favor, i.e. there is no (valid) entity or we don't have the storage force indexed
+    if not e.entity or not storage or not storage.forces[e.entity.force.index] then
+        return
+    end
+
+    -- Log the damage
+    damage.log(e.entity.force.index, game.tick, e.entity.surface.index, math.floor(e.entity.position.x),
+        math.floor(e.entity.position.y), e.final_damage_amount)
 end)
 
--- model/damage.lua
+++ model/damage.lua
@@ -1,38 +1,29 @@
 -- Data model
-storage = {
-    [force.index] = {
-        damage = {
-            [surface.index] = {
-                [x] = {
-                    [y] = {
-                        damage_decayed = double, -- The remaining decayed damage at the last_tick
-                        last_tick = int
-                    }
-                }
-            }
-        }
-    }
-}
-
+-- storage = {
+--     [force.index] = {
+--         damage = {
+--             [surface.index] = {
+--                 [x] = {
+--                     [y] = {
+--                         damage_decayed = double, -- The remaining decayed damage at the last_tick
+--                         last_tick = int
+--                     }
+--                 }
+--             }
+--         }
+--     }
+-- }
 -- Internal constants
 -- Some numbers for reference
 -- 30 minutes = 108.000 ticks
 -- 1 legendary gun turret (1000h) per minute for 30 minutes = 30.000 health
 -- 1 legendary wall (850h) per minute for 30 minutes = 26.250 health
 -- 1 normal wall (350h) per 5 minutes for 30 minutes = 2.100 health
-local DECAY_CONSTANT = 314159  -- bigger = slower decay
+local DECAY_CONSTANT = 314159 -- bigger = slower decay
 local MIN_TICK_DELTA = 60 -- Minimum tick delay before we need to start calculating the decay
 local MAX_TICK_DELTA = 108000 -- Maximum tick delay cut-off
-local kernel = {
-    {1, 2, 1},
-    {2, 4, 2},
-    {1, 2, 1}
-}
+local kernel = {{1, 2, 1}, {2, 4, 2}, {1, 2, 1}}
 local KSUM = 16
-
-
-
-
 
 local damage = {}
 --------------------------------------------------
@@ -40,20 +31,28 @@ local damage = {}
 --------------------------------------------------
 
 local get_storage = function(force_index)
+    if not storage.forces[force_index] then
+        return
+    end
     return storage.forces[force_index].damage
 end
 local get_cell = function(force_index, tick, surface_index, x, y)
     -- Ensure storage
     local dmg = get_storage(force_index)
+    if not dmg then
+        return
+    end
     dmg[surface_index] = dmg[surface_index] or {}
     dmg[surface_index][x] = dmg[surface_index][x] or {}
     dmg[surface_index][x][y] = dmg[surface_index][x][y] or {}
     return dmg[surface_index][x][y]
 end
 
-local get_decayed_damage(value, old_tick, new_tick)
+local get_decayed_damage = function(value, old_tick, new_tick)
     -- Early exit if no (positive) value was passed
-    if not value or value <= 0 then return 0 end
+    if not value or value <= 0 then
+        return 0
+    end
 
     -- Get time delta or early exit if outside of range
     local dt = new_tick - old_tick
@@ -69,8 +68,6 @@ local get_decayed_damage(value, old_tick, new_tick)
     return decayed
 end
 
-
-
 --------------------------------------------------
 -- Read
 --------------------------------------------------
@@ -78,56 +75,73 @@ end
 damage.get = function(force_index, surface_index, bounding_box)
     -- Get storage
     local dmg = get_storage(force_index)
-    if not dmg[surface_index] then return end
+    if not dmg or not dmg[surface_index] then
+        return
+    end
     local ds = dmg[surface_index]
 
-
     -- Get grid bounds
-    local minX =
-    local maxX =
-    local minY = 
-    local maxY =
+    local minX = bounding_box[1][1]
+    local maxX = bounding_box[2][1]
+    local minY = bounding_box[1][2]
+    local maxY = bounding_box[2][2]
+    -- game.print("BoundingBox=" .. serpent.line({{minX, minY}, {maxX, maxY}}))
     for x, col in pairs(ds) do
-        if x < minX then minX = x end
-        if x > maxX then maxX = x end
+        if x < minX then
+            minX = x
+        end
+        if x > maxX then
+            maxX = x
+        end
         for y, _ in pairs(col) do
-            if y < minY then minY = y end
-            if y > maxY then maxY = y end
+            if y < minY then
+                minY = y
+            end
+            if y > maxY then
+                maxY = y
+            end
         end
     end
 
     -- Make empty dense grid
     local mat = {}
-    for x = minX-1, maxX+1 do
+    for x = minX - 1, maxX + 1 do
         mat[x] = {}
-        for y = minY-1, maxY+1 do
+        for y = minY - 1, maxY + 1 do
             mat[x][y] = 0
         end
     end
 
     -- fill the grid
-    for x, col in pairs(ds) do
-        for y, data in pairs(col) double
-            mat[x][y] = get_decayed_damage(force_index, ds[x][y].last_tick, game.tick, ds[x][y].damage_decayed)
+    for x, col in pairs(ds or {}) do
+        for y, data in pairs(col or {}) do
+            if x > minX and x < maxX and y > minY and y < maxY then
+                local dam = get_decayed_damage(ds[x][y].damage_decayed, ds[x][y].last_tick, game.tick)
+                -- game.print("(" .. game.tick .. ") {" .. x .. "," .. y .. "} has dmg " .. dam .. " based on " ..
+                --                ds[x][y].damage_decayed .. " decayed damage at tick " .. ds[x][y].last_tick .. "(" ..
+                --                ds[x][y].last_tick - game.tick .. ")")
+                mat[x][y] = dam
+            end
         end
     end
 
     -- Gaussian blur the grid
     local out = {}
-    for x = minX-1, maxX+1 do
+    for x = minX - 1, maxX + 1 do
         out[x] = {}
-        for y = minY-1, maxY+1 do
+        for y = minY - 1, maxY + 1 do
             local sum = 0
             for ky = -1, 1 do
                 for kx = -1, 1 do
                     local yy = y + ky
                     local xx = x + kx
-                    if mat[yy] and mat[yy][xx] then
-                        sum = sum + mat[yy][xx] * kernel[ky+2][kx+2]
+                    if mat[xx] and mat[xx][yy] then
+                        sum = sum + mat[xx][yy] * kernel[kx + 2][ky + 2]
                     end
                 end
             end
-            out[y][x] = sum / KSUM
+            local dam = sum / KSUM
+            out[x][y] = dam
         end
     end
 
@@ -141,19 +155,27 @@ end
 damage.log = function(force_index, tick, surface_index, x, y, damage_amount)
     -- Get storage cell
     local dmg = get_cell(force_index, tick, surface_index, x, y)
+    if not dmg then
+        return
+    end
 
     -- Update value decayed based on delta last tick
     local decayed_damage = 0
-    if dmg[x]
-    dmg[x][y].damage_decayed = decayed_damage + damage_amount
-    dmg[x][y].last_tick = tick
+    if dmg.damage_decayed then
+        decayed_damage = get_decayed_damage(dmg.damage_decayed, dmg.last_tick, game.tick)
+    end
+    dmg.damage_decayed = decayed_damage + damage_amount
+    dmg.last_tick = tick
 end
 
-
 damage.init = function()
-    for _,p in pairs(game.players) do
-        storage.forces[p.force.index] = storage.forces[p.force.index] or {}
-        storage.forces[p.force.index].damage = storage.forces[p.force.index].damage or {}
+    for _, p in pairs(game.players) do
+        if not storage.forces[p.force.index] then
+            storage.forces[p.force.index] = {}
+        end
+        if not storage.forces[p.force.index].damage then
+            storage.forces[p.force.index].damage = {}
+        end
     end
 end
 
